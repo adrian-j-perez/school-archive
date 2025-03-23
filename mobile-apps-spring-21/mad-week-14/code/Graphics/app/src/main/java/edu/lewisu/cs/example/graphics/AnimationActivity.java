eb43bbd8ea78d279fa7c9fcc3d5415cfb10004b6
Adrian <perez324158@gmail.com>
2025-03-22 23:08:49 -0500

    add other app

---
+++ mobile-apps-spring-21/mad-week-14/code/Graphics/app/src/main/java/edu/lewisu/cs/example/graphics/AnimationActivity.java
@@ -0,0 +1,15 @@
+package edu.lewisu.cs.example.graphics;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AnimationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animation);
+
+    }
+}
